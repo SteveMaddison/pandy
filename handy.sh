38a5cb67997fc4406f60881f7ba3aaac71ce10c0
Steve Maddison <steve@cosam.org>
2010-06-16 14:24:08 +0000

    Added Pandora PND package files.

---
+++ handy.sh
@@ -0,0 +1,19 @@
+#?bin/sh
+
+BIOS=lynxboot.img
+
+if [ ! -f ./$BIOS ] ; then
+	cp "`zenity --file-selection --title="Select your $BIOS file"`" $BIOS
+	if [ $? != 0 ] ; then
+		zenity --error --text="Sorry, emulator cannot run without a BIOS file."
+		exit 1
+	fi
+	md5sum -c lynxboot.md5 || zenity --question --text="BIOS does not appear to be the correct version. Use it anyway?"
+	if [ $? != 0 ] ; then
+		rm -f $BIOS
+		exit 1
+	fi
+fi
+
+./handy_sdl
+
